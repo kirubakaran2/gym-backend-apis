4a13159d9838d9103c9ea31f13124821da9eb24c
Rakesh Kumar <70006331+0xrakesh@users.noreply.github.com>
2024-05-26 12:17:45 +0530

    Add punch in/out get api

--- Router/punch.js
+++ Router/punch.js
@@ -4,10 +4,12 @@ const Customer = require("../Schema/customer")
 const {messager} = require("./sender")
 
 exports.intime = async(req,res) => {
-
     let now = new Date();
     const {id} = req.body;
     const user = await Customer.findOne({ID:id});
+    if(!user) {
+        return res.status(404).json({status:"User not found."})
+    }
 
     let a = new Date();
     let dateString = `${a.getUTCFullYear()}-${(a.getUTCMonth() + 1).toString().padStart(2, "0")}-${a.getUTCDate().toString().padStart(2, "0")}`;
@@ -18,8 +20,7 @@ exports.intime = async(req,res) => {
     if(Checking)
         return res.json({status:"You already punch in for today."});
 
-    if(!user)
-        return res.status(404).status({status:"User not found."})
+
     const UserPunch = new punch({
         CUSTOMER_PROFILE_ID: id,
         IN_TIME: now,
@@ -38,7 +39,7 @@ exports.intime = async(req,res) => {
     
         Keep pushing,
         Titanfitnessstudio`
-        messager(msg,user.PHONE,'in time entry message.')
+        // messager(msg,user.PHONE,'in time entry message.')
         return res.status(200).json({status:"In time entried."})
     }).
     catch(() => {
@@ -50,6 +51,9 @@ exports.outTime = async(req,res) => {
     const {id} = req.body;
     let a = new Date();
     const user = await Customer.findOne({ID:id});
+    if(!user) {
+        return res.status(302).json({status:"User id not found"})
+    }
     let dateString = `${a.getUTCFullYear()}-${(a.getUTCMonth() + 1).toString().padStart(2, "0")}-${a.getUTCDate().toString().padStart(2, "0")}`;
     let date = new Date(dateString);
 
@@ -70,11 +74,80 @@ exports.outTime = async(req,res) => {
         
         Keep pushing,
         Titalfitnessstudio`
-        messager(msg,user.PHONE,'in time entry message.')
+        // messager(msg,user.PHONE,'in time entry message.')
         return res.status(200).json({status:"Out Time Entry"})
     }).
     catch((err) => {
         return res.status(500).json({status:"Internal Server Error",error:err})
     });
 
+}
+
+exports.getIn = async(req,res) => {
+    const userId = req.query.userId;
+    let today = new Date();
+    try {
+        if(userId) {
+            let timing = await punch.findOne({CUSTOMER_PROFILE_ID:userId,
+                $expr: {
+                    $and: [
+                        { $eq: [{ $dayOfMonth: "$CREATED_DATE" }, today.getDate()] },
+                        { $eq: [{ $month: "$CREATED_DATE" }, today.getMonth() + 1] },
+                        { $eq: [{ $year: "$CREATED_DATE"}, today.getUTCFullYear()]}
+                    ]
+                }
+            },{OUT_TIME:0});
+            if(timing===null)
+                timing = [];
+            return res.status(200).json({timing:timing});
+        }
+        let timing = await punch.find({
+            $expr: {
+                $and: [
+                    { $eq: [{ $dayOfMonth: "$CREATED_DATE" }, today.getDate()] },
+                    { $eq: [{ $month: "$CREATED_DATE" }, today.getMonth() + 1] },
+                    { $eq: [{ $year: "$CREATED_DATE"}, today.getUTCFullYear()]}
+                ]
+            }
+        },{OUT_TIME:0})
+        return res.status(200).json({timing: timing});
+    }
+    catch(err) {
+        console.log(err)
+        return res.status(500).json({status:"Internal Server Error",error:err})
+    }
+}
+
+exports.getOut = async(req,res) => {
+    const userId = req.query.userId;
+    let today = new Date();
+    try {
+        if(userId) {
+            let timing = await punch.findOne({CUSTOMER_PROFILE_ID:userId,
+                $expr: {
+                    $and: [
+                        { $eq: [{ $dayOfMonth: "$CREATED_DATE" }, today.getDate()] },
+                        { $eq: [{ $month: "$CREATED_DATE" }, today.getMonth() + 1] },
+                        { $eq: [{ $year: "$CREATED_DATE"}, today.getUTCFullYear()]}
+                    ]
+                }
+            });
+            if(timing===null)
+                timing = [];
+            return res.status(200).json({timing:timing});
+        }
+        let timing = await punch.find({
+            $expr: {
+                $and: [
+                    { $eq: [{ $dayOfMonth: "$CREATED_DATE" }, today.getDate()] },
+                    { $eq: [{ $month: "$CREATED_DATE" }, today.getMonth() + 1] },
+                    { $eq: [{ $year: "$CREATED_DATE"}, today.getUTCFullYear()]}
+                ]
+            }
+        })
+        return res.status(200).json({timing: timing});
+    }
+    catch(err) {
+        return res.status(500).json({status:"Internal Server Error"})
+    }
 }
